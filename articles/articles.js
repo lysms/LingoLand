b9b48daea697087462a9f07f89c0ec6e3ab3f591
librautoo <wangy63@rpi.edu>
2020-10-06 15:56:16 +0800

    update article page

---
+++ articles/articles.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    // Click function for changing article genre and language
+    $("#English").click(function() {
+        $("#dropdownMenuButton").text("English");
+        // document.getElementsByClassName("LanguageDisplay").innerHTML = "English-Lol"; This method doesnt work
+        alert("Look! English-Lol");
+    });
+
+    $("#Germany").click(function() {
+        $("#dropdownMenuButton").text("Deutsch");
+        alert("Aussehen! Deustch-Lol");
+    });
+
+    $("#Chinese").click(function() {
+        $("#dropdownMenuButton").text("中文");
+        alert("看! 中文-哈哈");
+    });
+
+    $("#Technology").click(function() {
+        $("#dropdownMenuButtonPri").text("Technology");
+        alert("Look! Technology Coming up!");
+    });
+
+    $("#Art-and-Culture").click(function() {
+        $("#dropdownMenuButtonPri").text("Art and Culture");
+        alert("Look! HASS Coming up!");
+    });
+
+    $("#Politics").click(function() {
+        $("#dropdownMenuButtonPri").text("Trumpian");
+        alert("Look! Trump is Coming up!");
+    });
+});
