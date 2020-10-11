a95dac7644125b4be3ca3c4afded25eb2c182f22
neptune7000 <aviss@rpi.edu>
2020-10-11 14:07:06 -0400

    Fixes Flashcard CSS+JS

--- flashcards/flashcards.php
+++ flashcards/flashcards.php
@@ -18,33 +18,16 @@
   </section>
   
   <main>
-    <h2 class="card" id="question">This is an example question in the english language. It is here in order to test text alignment of the CSS.</h2>
-    <h2 class="card" id="answer">example: [definition of example in NL]</h2>
+    <h2 class="flashcard" id="question">This is an example question in the english language. It is here in order to test text alignment of the CSS.</h2>
+    <h2 class="flashcard" id="answer">example: [definition of example in NL]</h2>
   </main>
 
-  <button type="button" id="show_button" onclick="showAnswer()">Show Answer</button>
-  <section id="answer_box">
-  	<button type="button" class="answer_button" onclick="answer()">Again</button>
- 		<button type="button" class="answer_button" onclick="answer()">Correct</button>
+  <section id="user_input">
+    <button type="button" id="show_button" onclick="showAnswer()">Show Answer</button>
+    <button type="button" id="edit_button">Edit Card</button>
   </section>
-  
 
-  <button type="button" id="edit_button">Edit Card</button>
-
-
-  <!-- basic temp functionality -->
-  <script>
-    function showAnswer() {
-      document.getElementById("answer").style.visibility = "visible";
-      document.getElementById("show_button").style.visibility = "hidden";
-      document.getElementById("answer_box").style.visibility = "visible";
-    }
-    function answer() {
-      document.getElementById("answer").style.visibility = "hidden";
-      document.getElementById("show_button").style.visibility = "visible";
-      document.getElementById("answer_box").style.visibility = "hidden";
-    }
-  </script>
+  <script src="flashcards.js"></script>
 </div>
   <?php include('../includes/foot.inc.php');
   // footer info and closing tags
