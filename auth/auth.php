7b0a5bea5e4ff3aec37cd776f7e5a32ab4441125
Yanshen Lin <49962797+l-sms-yanshen@users.noreply.github.com>
2020-11-23 11:30:07 -0500

    Updated

--- auth/auth.php
+++ auth/auth.php
@@ -54,31 +54,7 @@ include('connection.php');
     </div>
 </div>
 <div>
-    <!-- <div class="sidenav">
-        <div class="login-main-text">
-            <h2>LingoLand<br> Login Page</h2>
-            <p>Login or register from here to access.</p>
-        </div>
-    </div> -->
-    <!-- <div class="main-auth">
-        <div class="col-md-6 col-sm-12">
-            <div class="login-form">
-                <form action="auth.php" method="post">
-                    <div class="form-group">
-                        <label for="username">User Name</label>
-                        <input type="text" name="username" class="form-control" placeholder="User Name" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
-                    <input type="submit" value="Login" name="login" class="btn btn-black" />
-                    <a href="register.php" class="btn btn-secondary">Register</a>
-                </form>
-            </div>
-        </div>
-        <div class="whitespace"></div>
-    </div> -->
+
     <?php include('../includes/foot.inc.php');
     // footer info and closing tags
     ?>
--- auth/register.php
+++ auth/register.php
@@ -5,67 +5,13 @@ include('connection.php');
 ?>
 
 <title>Register</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="main.css">
 <scrip src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></scrip>
 <!------ Include the above in your HEAD tag ---------->
 
 <?php include('../includes/head.inc.php'); ?>
-
-<!-- <div class="sidenav">
-        <div class="login-main-text">
-            <h2>LingoLand<br> Registration Page</h2>
-            <p>Enter information here.</p>
-        </div>
-    </div> -->
-<!-- <div class="main-auth">
-        <div class="col-md-6 col-sm-12">
-            <div class="register-form">
-                <form action="register.php" method="post">
-
-
-                    <div class="form-group">
-                        <label>User Name</label>
-                        <input type="text" name="username" class=" form-control" placeholder="User Name" required>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <label for="firstname">First Name</label>
-                            <input type="firstname" name="firstname" class="form-control" placeholder="First Name"
-                                required>
-                        </div>
-                        <div class="col-sm-6">
-                            <label for="lastname">Last Name</label>
-                            <input type="lastname" name="lastname" class="form-control" placeholder="Last Name"
-                                required>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password1">Confirm Password</label>
-                        <input type="password" name="password1" class="form-control" placeholder="Password" required>
-                    </div>
-
-
-                    <select name="language">
-                        <option value="" disabled selected>Choose your Language</option>
-                        <option value="spanish">Spanish</option>
-                        <option value="chinese">Chinese</option>
-                        <option value="italian">Italian</option>
-                    </select>
-
-                    <div class="form-group">
-                        <button type="submit" name="register" class="btn btn-black">Register</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div> -->
-
-
 
 <div class="container">
     <div class="myCard">
@@ -76,30 +22,35 @@ include('connection.php');
                         <header>Create Account</header>
                         <div class="form-group">
                             <i class="fas fa-user"></i>
-                            <input class="myInput" type="text" name="username" placeholder="Username" id="username" required>
+                            <input class="myInput" type="text" name="username" placeholder="Username" id="username"
+                                required>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
 
                         <div class="form-group">
                             <i class="fas fa-user-circle"></i>
-                            <input class="myInput" type="text" name="firstname" placeholder="First Name" id="firstname" required>
+                            <input class="myInput" type="text" name="firstname" placeholder="First Name" id="firstname"
+                                required>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
 
                         <div class="form-group">
                             <i class="fas fa-user-circle"></i>
-                            <input class="myInput" type="text" name="lastname" placeholder="Last Name" id="lastname" required>
+                            <input class="myInput" type="text" name="lastname" placeholder="Last Name" id="lastname"
+                                required>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
 
                         <div class="form-group">
                             <i class="fas fa-lock"></i>
-                            <input class="myInput" placeholder="Password" type="password" name="password" id="password" required>
+                            <input class="myInput" placeholder="Password" type="password" name="password" id="password"
+                                required>
                         </div>
 
                         <div class="form-group">
                             <i class="fas fa-lock"></i>
-                            <input class="myInput" placeholder="Confirm Password" type="password" name="password1" id="password" required>
+                            <input class="myInput" placeholder="Confirm Password" type="password" name="password1"
+                                id="password" required>
                         </div>
 
                         <input type="submit" class="butt" value="CREATE ACCOUNT" name="register">
